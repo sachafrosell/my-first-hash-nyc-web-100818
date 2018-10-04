1a8d64ab57cb7e5ccda4eec4a7d67c298171324f
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 16:20:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,14 +27,9 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2 
-  shipping_manifest[]
+  shipping_manifest["gun powder"] = 4
 
+  shipping_manifest
 
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
 
 end
